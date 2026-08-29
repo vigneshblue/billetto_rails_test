3d2c4733937d0de3add92913b7f8a39fe601162d
Vigneshwaran Sivalingam <vigneshsika@gmail.com>
2026-08-29 17:21:36 +0530

    Add upvote downvote UI, routes, signed in logic

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,7 +1,22 @@
 class EventsController < ApplicationController
-  before_action :require_clerk_session!
+  before_action :require_clerk_session!, except: [:index]
+  before_action :set_event_and_user, only: [:upvote, :downvote]
 
   def index
     @pagy, @events = pagy(:offset, Event.all.ordered)
   end
+
+  def upvote    
+  end
+
+  def downvote
+  end
+
+  private
+
+    def set_event_and_user
+      @event = Event.find(params[:id])
+      @event_id = @event.event_id
+      @user_id = @current_user.user_id
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,10 @@ Rails.application.routes.draw do
   get "/sign_up" => "clerk#sign_up"
   get "/sign_in" => "clerk#sign_in"
   get "/sign_out" => "clerk#sign_out"
-  resources :events, only: [:index]
+  resources :events, only: [:index] do
+    post :upvote, on: :member
+    post :downvote, on: :member
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
