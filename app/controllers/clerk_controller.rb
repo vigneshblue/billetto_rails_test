33d311e4ed51213fea08b5ba7d019e2c62a2b817
Vigneshwaran Sivalingam <vigneshsika@gmail.com>
2026-09-01 16:54:05 +0530

    Add redirect for signed in users when visiting sign_in/sign_up

--- app/controllers/clerk_controller.rb
+++ app/controllers/clerk_controller.rb
@@ -1,4 +1,6 @@
 class ClerkController < ApplicationController
+  before_action :redirect_if_signed_in, only: [:sign_up, :sign_in]
+
   def sign_up
   end
 
@@ -7,4 +9,10 @@ class ClerkController < ApplicationController
 
   def sign_out
   end
+
+  private
+
+    def redirect_if_signed_in
+      redirect_to root_path if @current_user
+    end
 end
